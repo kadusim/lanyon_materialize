5ab2a0b306f4d2cc9dba4dafb0b0458a66348382
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-03-09 20:24:13 -0300

    Add animate button-collapse

--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,16 @@
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav({
-      menuWidth: 300, // dafault
-      draggable: true,
-      onOpen: function(el) { openNavBar() },
-      onClose: function(el) { closeNavBar() }
-    });
+$(function(){
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // dafault
+    draggable: true,
+    onOpen: function(el) { openNavBar() },
+    onClose: function(el) { closeNavBar() }
   }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-// $('.button-collapse').sideNav();
+});
 
 function openNavBar() {
-  $("main").css({
-    "padding-left":"300px"
-  });
+  $("main").animate({ "padding-left": "+=300px" }, 200 );
 }
 
 function closeNavBar() {
-  $("main").css({
-    "padding-left":"0px"
-  });
+  $("main").animate({ "padding-left": "-=300px" }, 200 );
 }
